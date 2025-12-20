b10fdfc1dade8c5c3f6ee6bf5f5b13c5b09b4c1f
Christof Reimers <contact@peatral.xyz>
2025-12-21 00:28:58 +0100

    cleanup menu

--- multiplayer_menu.gd
+++ multiplayer_menu.gd
@@ -3,12 +3,13 @@ extends Control
 @export var ip_address_edit: LineEdit
 @export var main_scene: PackedScene
 
-func _on_client_button_pressed() -> void:
+
+func _on_server_button_pressed() -> void:
 	NetworkManager.create_server()
 	get_tree().change_scene_to_packed(main_scene)
 
 
-func _on_server_button_pressed() -> void:
+func _on_client_button_pressed() -> void:
 	var ip_address = ip_address_edit.text
 	NetworkManager.connect_to_server(ip_address)
 	get_tree().change_scene_to_packed(main_scene)
