ef844c6ccb5288a3678378337c285a8a8e4c4921
Christof Reimers <contact@peatral.xyz>
2025-12-21 00:35:23 +0100

    apply window sizes

--- multiplayer_menu.gd
+++ multiplayer_menu.gd
@@ -6,10 +6,14 @@ extends Control
 
 func _on_server_button_pressed() -> void:
 	NetworkManager.create_server()
+	get_window().size = Vector2i(1920 * 3, 1080)
+	get_window().mode = Window.MODE_FULLSCREEN
 	get_tree().change_scene_to_packed(main_scene)
 
 
 func _on_client_button_pressed() -> void:
 	var ip_address = ip_address_edit.text
 	NetworkManager.connect_to_server(ip_address)
+	get_window().size = Vector2i(1920, 1080)
+	get_window().mode = Window.MODE_FULLSCREEN
 	get_tree().change_scene_to_packed(main_scene)
