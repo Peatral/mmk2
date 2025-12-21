bd9bc9c005698a9efe6cd8605d0acf354347c655
Christof Reimers <contact@peatral.xyz>
2025-12-21 22:56:44 +0100

    add headline data

---
+++ scripts/headlines/headline.gd
@@ -0,0 +1,21 @@
+class_name Headline
+extends Resource
+
+enum PoliticalOrientation {
+	Left = 0,
+	Center = 1,
+	Right = 2,
+}
+
+enum Realness {
+	Lie = 0,
+	Halftruth = 1,
+	Truth = 2
+}
+
+@export
+var text: String
+@export
+var politicalOrientation := PoliticalOrientation.Center
+@export
+var realness := Realness.Truth
