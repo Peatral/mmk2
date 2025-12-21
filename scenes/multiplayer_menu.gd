c510da675b2b325ce21c04218833dd2116998eeb
Christof Reimers <contact@peatral.xyz>
2025-12-22 00:27:47 +0100

    apply config on screen selection

--- scenes/multiplayer_menu.gd
+++ scenes/multiplayer_menu.gd
@@ -4,16 +4,29 @@ extends Control
 @export var main_scene: PackedScene
 
 
-func _on_server_button_pressed() -> void:
+func _create_server() -> void:
 	NetworkManager.create_server()
 	get_window().size = Vector2i(1920, 1080)
 	get_window().mode = Window.MODE_FULLSCREEN
 	get_tree().change_scene_to_packed(main_scene)
 
 
-func _on_client_button_pressed() -> void:
+func _connect_to_server() -> void:
 	var ip_address = ip_address_edit.text
 	NetworkManager.connect_to_server(ip_address)
 	get_window().size = Vector2i(1920, 1080)
 	get_window().mode = Window.MODE_FULLSCREEN
 	get_tree().change_scene_to_packed(main_scene)
+
+
+func _on_screen_button_pressed() -> void:
+	_create_server()
+	KinectAutoload.apply_calibration_for_front()
+
+func _on_floor_back_button_pressed() -> void:
+	_connect_to_server()
+	KinectAutoload.apply_calibration_for_left()
+
+func _on_floor_front_button_pressed() -> void:
+	_connect_to_server()
+	KinectAutoload.apply_calibration_for_right()
--- scripts/kinect_autoload.gd
+++ scripts/kinect_autoload.gd
@@ -8,15 +8,20 @@ var depth_texture := preload("res://assets/resources/depth_texture.tres")
 func start() -> void:
 	if not kinect.IsRunning:
 		kinect.Initialize(0)
-
-		# Right
-		kinect.Transform = Transform3D(Basis.from_euler(Vector3(0, -PI/2, 0)), Vector3(-1, 1, 0))
-		# Left
-		# kinect.Transform = Transform3D(Basis.from_euler(Vector3(0, PI/2, 0)), Vector3(1, 1, 0))
-		# Front
-		#kinect.Transform = Transform3D(Basis.from_euler(Vector3(0, 0, 0)), Vector3(0, 1, 0))
-
 		kinect.Start(data_reference, depth_texture, 0, 5000, false, true)
+	
+
+func apply_calibration_for_front() -> void:
+	kinect.Transform = Transform3D(Basis.from_euler(Vector3(0, 0, 0)), Vector3(0, 1, 0))
+	
+
+func apply_calibration_for_left() -> void:
+	kinect.Transform = Transform3D(Basis.from_euler(Vector3(0, PI/2, 0)), Vector3(1, 1, 0)) 
+	
+
+func apply_calibration_for_right() -> void:
+	kinect.Transform = Transform3D(Basis.from_euler(Vector3(0, -PI/2, 0)), Vector3(-1, 1, 0))
+	
 
 func _exit_tree() -> void:
 	kinect.Stop()
