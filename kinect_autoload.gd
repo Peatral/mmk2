e1ee4e4b8f40c6b16c6420d815c1e8982aab1be4
Christof Reimers <contact@peatral.xyz>
2025-12-21 01:08:46 +0100

    increase default max depth

--- kinect_autoload.gd
+++ kinect_autoload.gd
@@ -7,7 +7,7 @@ var depth_texture := preload("res://depth_texture.tres")
 
 func _enter_tree() -> void:
 	kinect.Initialize(0)
-	kinect.Start(tracker, depth_texture, 0, 2000, false, true)
+	kinect.Start(tracker, depth_texture, 0, 5000, false, true)
 
 func _exit_tree() -> void:
 	kinect.Stop()
