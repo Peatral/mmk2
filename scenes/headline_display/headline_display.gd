c3ce238a762956a28a5cc0d48b17f16c18913b31
Christof Reimers <contact@peatral.xyz>
2025-12-22 11:15:06 +0100

    add really basic voting

--- scenes/headline_display/headline_display.gd
+++ scenes/headline_display/headline_display.gd
@@ -1,5 +1,8 @@
 extends Control
 
+signal headline_vote_started(headline: Headline)
+signal headline_vote_stopped(headline: Headline)
+
 @export var headlines: HeadlineContainer = preload("res://assets/resources/headlines.tres")
 
 var current_headline: Headline
@@ -8,12 +11,15 @@ func _ready() -> void:
 	display_new_headline()
 
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	$VBoxContainer/TextureProgressBar.value = $Timer.time_left / $Timer.wait_time * 100
 
 
 func display_new_headline() -> void:
+	if current_headline:
+		headline_vote_stopped.emit(current_headline)
 	current_headline = headlines.headlines.pick_random()
+	headline_vote_started.emit(current_headline)
 	$VBoxContainer/Label.text = current_headline.text
 	
 
