4da4e4a358b6851161b22cbd7c9c31c9ca497881
Christof Reimers <contact@peatral.xyz>
2026-01-21 23:00:17 +0100

    add headline display with fog

---
+++ scenes/headline_display_fog/headline_display_fog.gd
@@ -0,0 +1,27 @@
+extends Control
+
+signal headline_vote_started(headline: Headline)
+signal headline_vote_stopped(headline: Headline)
+
+@export var headlines: HeadlineContainer = preload("res://assets/resources/headlines.tres")
+
+var current_headline: Headline
+
+func _ready() -> void:
+	display_new_headline()
+
+
+func _process(_delta: float) -> void:
+	$ProgressBar.value = ($Timer.wait_time - $Timer.time_left) / $Timer.wait_time * 100
+
+
+func display_new_headline() -> void:
+	if current_headline:
+		headline_vote_stopped.emit(current_headline)
+	current_headline = headlines.headlines.pick_random()
+	headline_vote_started.emit(current_headline)
+	$AspectRatioContainer2/CenterContainer/Label.text = current_headline.text
+	
+
+func _on_timer_timeout() -> void:
+	display_new_headline()
